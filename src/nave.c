ab44553970f2c1c1196862f193e88cdfab014902
bacobax <francesco.bassignana.info@gmail.com>
2022-12-08 16:16:59 +0100

    indicazione per fare la initShip

--- src/nave.c
+++ src/nave.c
@@ -7,8 +7,20 @@
 #include "../utils/support.h"
 #include "../utils/vettoriInt.h"
 #include "./nave.h"
+Ship* initShip() {
+    /*
+        La initShip falla come ho fatto la initPorto:
 
-int main(int argc, char* argv[]){
+        Port initPort(int sIndex , ...) {
 
-    
+            int shipShmId = useShm(SSHMKEY, SO_NAVI * sizeof(struct ship), errorHandler);
+
+            Ship* p = ((Ship*)getShmAddress(portShmId, 0, errorHandler)) + sIndex; //* per prendere la sIndex-esima nave dell'array in shm
+
+    */
+    return NULL;
+}
+int main(int argc, char* argv[]) {
+
+
 }
