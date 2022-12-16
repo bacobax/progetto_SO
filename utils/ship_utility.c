2dd92aa41ccc3cd94dd27bc8883699427ddee080
iliy27 <iliyan.kostadinov2705@gmail.com>
2022-12-16 10:26:32 +0100

    yes

--- utils/ship_utility.c
+++ utils/ship_utility.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../src/nave.h"
+#include "../src/porto.h"
 #include "../utils/loadShip.h"
 #include "../config1.h"
 #include "./support.h"
 #include <stdio.h>
+#include <signal.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 
 int checkCapacity(Ship ship) {
@@ -56,7 +60,7 @@ Ship initShip() {
 void printShip(void* ship, int id_ship){
     printf("[%d]: Nave\n", id_ship);
     
-    printf("coords: [x:%f, y:%f]\n", (((Ship)ship)->cords[0]), (((Ship)ship)->cords[1]));
+    printf("coords: [x:%f, y:%f]\n", (((Ship)ship)->x), (((Ship)ship)->y));
     
     printf("ton trasporati:%d\n", availableCapacity((Ship) ship));
 
@@ -65,4 +69,34 @@ void printShip(void* ship, int id_ship){
     
     printf("______________________________________________\n");
 
+}
+
+void travel(Ship ship, int portID){
+
+    Port p;
+    double dt_x, dt_y, result, nanosleep_arg;
+    struct timespec arg;
+
+    int portShmId = useShm(PSHMKEY, SO_PORTI * sizeof(struct port), errorHandler); /* prendo l'id della shm del porto */
+
+    p = ((Port) getShmAddress(portShmId, 0, errorHandler)) + portID; /* prelevo la struttura del porto alla portID-esima posizione nella shm */
+
+    /* imposto la formula per il calcolo della distanza*/
+    
+    dt_x = p->x - ship->x;
+    dt_y = p->y - ship->y;
+
+    result = pow(dt_x, 2) + pow(dt_y, 2);
+
+    arg.tv_sec = (time_t) (sqrt(result)) / SO_SPEED;
+    arg.tv_nsec = 0;
+
+    nanosleep(&arg, NULL);
+
+    /* Dopo aver fatto la nanosleep la nave si trova esattamente sulle coordinate del porto
+       quindi aggiorniamo le sue coordinate
+    */
+
+    ship->x = p->x;
+    ship->y = p->y;
 }
