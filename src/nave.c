9c71f1aff0b37ac13423f8eb3e28c8932a49346e
iliy27 <iliyan.kostadinov2705@gmail.com>
2022-12-20 12:09:19 +0100

    a

--- src/nave.c
+++ src/nave.c
@@ -10,6 +10,7 @@
 #include "../utils/shm_utility.h"
 #include "../utils/support.h"
 #include "../utils/vettoriInt.h"
+#include "../utils/msg_utility.h"
 #include "./dump.h"
 #include "./nave.h"
 #include "./porto.h"
@@ -35,39 +36,38 @@ void accessPort(int portID) {
 
 }
 
+void charge(Ship ship){
+
+    int quantityToCharge = chooseQuantityToCharge(); /* funzione che ritorna la quantità che la nave vuole caricare */
+
+    
+
+}
+
+void discharge(){
+
+}
+
 int main(int argc, char* argv[]) { /* mi aspetto che nell'argv avrò l'identificativo della nave (es: nave 0, nave 1, nave 2, ecc..)*/
 
     Ship ship;
     int ship_index, portID;
-    
+    int charge = 1;
+
     ship = initShip(); /* inizializzo struttura dati della nave ed eventuali handler per segnali*/
     ship_index = atoi(argv[1]);
 
     waitForStart();
 
-    while (1) { /* ciclo di operazione che esegue la nave fino a quando non viene killata dal master*/
-        
-        portID = choosePort(/* PARAMETRI IN VIA DI DEFINIZIONE*/); /* la nave sceglie il porto dove andare a scambiare*/
+    while (1) { 
 
-        /*
-            la funzione choosePort sarà da implementare costruendo un algoritmo che scelga un porto per
-            andare a caricare e/o scaricare merci
-        */
-
-        travel(ship, portID); /*  TO-DO */ 
-
-        /*
-             la funzione travel() utilizza la nanosleep() per spostare la nave alle coordinate del porto
-             una volta terminato l'algoritmo choosePort
-        */
-
-        accessPort(portID); /* TO-DO */
-
-        /*
-             la funzione accessPort() tenta l'accesso alla banchina del porto per far si che la
-             nave possa compiere le sue operazioni di carico/scarico
-        */
-
+        if(charge == 1){
+            charge(ship);
+            charge = 0;
+        } else {
+            discharge();
+            charge = 1;
+        }       
     }
 
     exit(EXIT_FAILURE); /* non deve mai raggiungere questa parte di codice*/
